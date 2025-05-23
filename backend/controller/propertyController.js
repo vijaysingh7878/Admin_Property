cfec0c3ff9a4ef2a81c09a2948674e456988e057
vijaysingh7878 <vijaysinghshekhawat9928@gmail.com>
2025-05-23 17:50:27 +0530

    edit

--- backend/controller/propertyController.js
+++ backend/controller/propertyController.js
@@ -128,7 +128,7 @@ class propertyController {
                 if (query.property_Category) {
                     conditions.push({ category: query.property_Category });
                 }
-            
+
                 const matchQuery = conditions.length > 0 ? { $and: conditions } : {};
 
                 let sortOption = { createdAt: -1 };
@@ -322,6 +322,9 @@ class propertyController {
                             }
                             if (data.num == 'soon') {
                                 updateProperty.status = 'soon'
+                            }
+                            if (data.num == 'popular') {
+                                updateProperty.status = 'popular'
                             }
                             await propertyModel.updateOne(
                                 {
